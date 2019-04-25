b8b00ae7163616e38ce6cef9c516f040aea48406
Alexandra Nadova <alexandranadova@gmail.com>
2019-04-25 12:11:44 +0200

    registration and profile component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { FooterComponent } from './footer/footer.component';
+import { RegistrationComponent } from './user/registration/registration.component';
+import { ProfileComponent } from './user/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
     FooterComponent,
+    RegistrationComponent,
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
