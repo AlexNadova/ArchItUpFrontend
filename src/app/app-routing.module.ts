34eb01fee32c70c802bc13b533a8a5fc928e6baf
Alexandra Nadova <alexandranadova@gmail.com>
2019-04-25 18:57:55 +0200

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
+//import  components we want to route
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegistrationComponent} from './user/registration/registration.component';
+import { ProfileComponent} from './user/profile/profile.component';
 
-const routes: Routes = [];
+//define routes
+//path:'',   -page that loads when app loads - for Home page
+const routes: Routes = [
+  {
+    path: 'register', // e.g.: architup.sk/register
+    component: RegistrationComponent //component for path
+  },
+  {
+    path: 'profile/:id',
+    component: ProfileComponent 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
