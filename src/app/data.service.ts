44b333e1448f6144dfc96b17bd726f5069704b70
Alexandra Nadova <alexandranadova@gmail.com>
2019-04-25 20:35:21 +0200

    get data from random api

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,10 +1,20 @@
 //for reusable code that more components can access; e.g. connect to API to get data from DB
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  //getting code example:
+  getUsers(){
+    return this.http.get('https://jsonplaceholder.typicode.com/users')
+  }
+  //for profile comp.
+  getUser(userId){
+    return this.http.get('https://jsonplaceholder.typicode.com/users/'+userId)
+  }
 }
