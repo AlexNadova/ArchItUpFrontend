d13c4b3759c68d8798efdb232418b83212706066
Alexandra Nadova <alexandranadova@gmail.com>
2019-04-25 12:07:20 +0200

    deleted components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
-import { ProjectComponent } from './project/project.component';
-import { ArticleComponent } from './article/article.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent,
-    ProjectComponent,
-    ArticleComponent,
     NavigationBarComponent,
-    FooterComponent
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
