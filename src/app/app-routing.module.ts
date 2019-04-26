42956a6847b2d6faf31ae53b73523dc7205781f6
Alexandra Nadova <alexandranadova@gmail.com>
2019-04-26 09:48:00 +0200

    routing register and profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,27 @@ import { Routes, RouterModule } from '@angular/router';
 import { RegistrationComponent} from './user/registration/registration.component';
 import { ProfileComponent} from './user/profile/profile.component';
 
-//define routes
-//path:'',   -page that loads when app loads - for Home page
+//define routes 
 const routes: Routes = [
+  // {
+  //   path: '', //-page that loads when app loads - for Home page
+  //   component: HomeComponent,
+  //   data:{
+  //     title:'Home page'
+  //   },
   {
     path: 'register', // e.g.: architup.sk/register
-    component: RegistrationComponent //component for path
+    component: RegistrationComponent, //component for path
+    data:{
+      title:'Registration'
+    },
   },
   {
     path: 'profile/:id',
-    component: ProfileComponent 
+    component: ProfileComponent,
+    data:{
+      title:'Profile'
+    },
   }
 ];
 
