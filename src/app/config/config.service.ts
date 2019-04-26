6b6bcc26828d0def76294e368ffe6cd84a7c11a9
Alexandra Nadova <alexandranadova@gmail.com>
2019-04-26 13:39:54 +0200

    added configuration code

--- src/app/config/config.service.ts
+++ src/app/config/config.service.ts
@@ -1,22 +1,30 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+//separate data access logic. component will be easier to test and understand. this code can be reused
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpResponse } from "@angular/common/http";
+import { Observable } from "rxjs";
 
-@Injectable({
+export interface Config {
+  registerUrl: string;
+  textfile: string;
+}
+
+/**@Injectable({
   providedIn: 'root'
-})
+}) */
+@Injectable()
 export class ConfigService {
   configUrl = "assets/config.json";
+
   constructor(private http: HttpClient) {}
 
+  //get config.js file; with <Config> we can tell HttpClient the type of response to make consuming the output easier and more obvious.
   getConfig() {
-    return this.http.get(this.configUrl);
+    return this.http.get<Config>(this.configUrl);
   }
 
-  showConfig() {
-    this.configService.getConfig()
-      .subscribe((data: Config) => this.data = {
-          registerUrl: data['heroesUrl'],
-          textfile:  data['textfile']
-      });
-  }
+  //The response body doesn't return all the data you may need. Sometimes servers return special headers or status codes to indicate 
+  //certain conditions that are important to the application workflow. Tell HttpClient that you want the full response with the observe option:
+  getConfigResponse(): Observable<HttpResponse<Config>> {
+    return this.http.get<Config>(this.configUrl, { observe: "response" });
+  } //Now HttpClient.get() returns an Observable of typed HttpResponse rather than just the JSON data.
 }
