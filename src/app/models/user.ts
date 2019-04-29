b5fec9fcc7e19ac90ca46dc389d4c3611158a69f
Alexandra Nadova <alexandranadova@gmail.com>
2019-04-29 14:04:00 +0200

    registration with form works

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,7 +1,6 @@
 export class User {
   constructor(
     //public profile_picture:??
-    public id: number,
     public first_name: string,
     public last_name: string,
     public password: string,
--- src/app/user/registration/registration.component.ts
+++ src/app/user/registration/registration.component.ts
@@ -4,14 +4,6 @@ import { User } from "src/app/models/user"; //imports user model
 import { NgForm } from "@angular/forms";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 
-
-  const httpheaders= new HttpHeaders({
-    "Content-Type": "application/json",
-    "Authorization": "bestsecretever",
-    "Access-Control-Allow-Origin": "localhost:4200/register"
-  })
-
-
 //The @Component selector value of "app-registration" means you can drop this form in a parent template with a <app-registration> tag.
 @Component({
   selector: "app-registration",
@@ -20,39 +12,18 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 })
 export class RegistrationComponent implements OnInit {
   //users$: Object;
-  model = new User(
-    1,
-    "Alex",
-    "Nadova",
-    "Password7",
-    "alexandranadova@gmail.com",
-    "0917 930 891"
-  );
+  model = new User("","","","","");
   submitted = false;
 
   constructor(private data: UserService, private http: HttpClient) {}
-  user = {
-    firstName: "frontend",
-    lastName: "user",
-    email: "f.user@mail.com",
-    password: "faasdeDD5",
-    phone: "1111220"
-  };
 
   onSubmit(form: NgForm) {
     this.submitted = true;
-    //this.data.register(form);
-    return this.http
-      .post("localhost:4000/api/user/signup", this.user,{headers:httpheaders})
-      .subscribe(err => {
-        if (err) console.log(err);
-        console.log("Success");
-      });
-    console.log(form.value);
+    this.data.register(form);
   }
 
   newUser() {
-    this.model = new User(2, "", "", "", "", "");
+    this.model = new User("","", "", "", "");
   }
 
   // TODO: Remove this when we're done
