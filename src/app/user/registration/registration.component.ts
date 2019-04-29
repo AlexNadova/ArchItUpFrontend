bfcf2017f3fb1669870e1206b3c7f1fda8b001e0
Alexandra Nadova <alexandranadova@gmail.com>
2019-04-29 12:13:25 +0200

    register changed

--- src/app/user/registration/registration.component.ts
+++ src/app/user/registration/registration.component.ts
@@ -4,13 +4,13 @@ import { User } from "src/app/models/user"; //imports user model
 import { NgForm } from "@angular/forms";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 
-const httpOptions = {
-  headers: new HttpHeaders({
+
+  const httpheaders= new HttpHeaders({
     "Content-Type": "application/json",
-    //'Authorization': 'bestsecretever',
+    "Authorization": "bestsecretever",
     "Access-Control-Allow-Origin": "localhost:4200/register"
   })
-};
+
 
 //The @Component selector value of "app-registration" means you can drop this form in a parent template with a <app-registration> tag.
 @Component({
@@ -31,23 +31,23 @@ export class RegistrationComponent implements OnInit {
   submitted = false;
 
   constructor(private data: UserService, private http: HttpClient) {}
+  user = {
+    firstName: "frontend",
+    lastName: "user",
+    email: "f.user@mail.com",
+    password: "faasdeDD5",
+    phone: "1111220"
+  };
 
   onSubmit(form: NgForm) {
     this.submitted = true;
     //this.data.register(form);
-    this.http
-      .post(
-        "localhost:4000/api/user/signup",
-        {
-          'firstName': 'u4ser1',
-          'lastName': 'lutse',
-          'email': 'useraadada@mail.com',
-          'password': 'faasdeDD5',
-          'phone': '111122'
-        },
-        httpOptions
-      )
-      .subscribe();
+    return this.http
+      .post("localhost:4000/api/user/signup", this.user,{headers:httpheaders})
+      .subscribe(err => {
+        if (err) console.log(err);
+        console.log("Success");
+      });
     console.log(form.value);
   }
 
