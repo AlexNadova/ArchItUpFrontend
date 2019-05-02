ffe26cb9a77b0638fb473abab0ea29a177a7d69e
Alexandra Nadova <alexandranadova@gmail.com>
2019-05-02 17:12:00 +0200

    registration form updated

--- src/app/user/registration/registration.component.ts
+++ src/app/user/registration/registration.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from "@angular/core";
 import { UserService } from "../user.service";
-import { User } from "src/app/models/user"; //imports user model
 import { NgForm } from "@angular/forms";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 import { NavigationBarService } from "src/app/navigation-bar/navigation-bar.service";
 import { FooterService } from "src/app/footer/footer.service";
+import { Router } from "@angular/router";
 
 //The @Component selector value of "app-registration" means you can drop this form in a parent template with a <app-registration> tag.
 @Component({
@@ -14,13 +14,12 @@ import { FooterService } from "src/app/footer/footer.service";
   styleUrls: ["./registration.component.css"]
 })
 export class RegistrationComponent implements OnInit {
-  //users$: Object;
-  model = new User("", "", "", "", "");
+  //when form is submitted hide it
   submitted = false;
 
   constructor(
     private userService: UserService,
-    private http: HttpClient,
+    private router: Router,
     private nav: NavigationBarService,
     private footer: FooterService
   ) {}
@@ -30,22 +29,13 @@ export class RegistrationComponent implements OnInit {
     this.userService.register(form);
   }
 
-  newUser() {
-    this.model = new User("", "", "", "", "");
+//used when you click on Login button after registration is done. for now it loads home page and it needs to show footer and navbar
+  showAll() {
+    this.nav.show();
+    this.footer.show();
   }
-
-  // TODO: Remove this when we're done
-  //diagnostic property to return a JSON representation of the model.
-  get diagnostic() {
-    return JSON.stringify(this.model);
-  }
-
   //code executed when this component loads
   ngOnInit() {
-    /*get users example: 
-    this.data.getUsers().subscribe(data=> this.users$ =data)
-    this would be for one in profile: this.data.getUser().subscribe(data=> this.users$ =data)
-    */
     this.nav.hide();
     this.footer.hide();
   }
