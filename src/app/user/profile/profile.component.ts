7daf7e4031efc531eec0faadfbf940477d95efa0
Alexandra Nadova <alexandranadova@gmail.com>
2019-05-13 13:32:30 +0200

    variables given values

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 import { UserService } from "../user.service";
 import { User, Education, Experience } from "src/app/models/user";
 import { FooterService } from "src/app/footer/footer.service";
@@ -9,18 +10,33 @@ import { FooterService } from "src/app/footer/footer.service";
   styleUrls: ["./profile.component.css"]
 })
 export class ProfileComponent implements OnInit {
-  user: User;
+  user: User={
+    _id: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    //password: "",
+    phone: "",
+    country: "",
+    city: "",
+    //permissionLevel: { type: Number, default: config.permissionLevels.REG_USER },
+     fieldOfFocus: "",
+     education: []=[],
+     workExperience: []=[]
+  };
   currentUser;
-  education: Education[];
-  experience: Experience[];
+  education: Education[]=[];
+  experience: Experience[]=[];
   constructor(
     private userService: UserService,
-    private footer: FooterService
+    private footer: FooterService,
+    private router: Router
   ) {}
 
   getUserProfile() {
     this.userService.getUser().subscribe((data: User) => {
       this.user = { ...data };
+      console.log(this.user);
       this.education = this.user.education;
       this.experience = this.user.workExperience.sort(function(obj1, obj2) {
         return obj2.yearEnd - obj1.yearEnd;
@@ -34,6 +50,7 @@ export class ProfileComponent implements OnInit {
       console.log("Success");
     });
     this.userService.logout();
+    this.router.navigate(["login"]);
   }
 
   ngOnInit() {
