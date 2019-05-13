64be8ae023e780663ba7610744002d99218f1db1
Alexandra Nadova <alexandranadova@gmail.com>
2019-05-13 13:30:45 +0200

    get id, implemented delete, improved all methods

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -2,8 +2,7 @@ import { Injectable } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { User } from "../models/user";
-import { Observable, BehaviorSubject } from "rxjs";
-import { map } from "rxjs/operators";
+import { Observable} from "rxjs";
 
 const httpheaders = new HttpHeaders({
   "Content-Type": "application/json"
@@ -17,15 +16,8 @@ const registerUrl = "http://localhost:4000/api/user/signup";
   providedIn: "root"
 })
 export class UserService {
-  private currentUserSubject: BehaviorSubject<User>;
-  public currentUser: Observable<User>;
 
-  constructor(private http: HttpClient) {
-    this.currentUserSubject = new BehaviorSubject<User>(
-      JSON.parse(localStorage.getItem("currentUser"))
-    );
-    this.currentUser = this.currentUserSubject.asObservable();
-  }
+  constructor(private http: HttpClient) {}
   id: String;
   user = {
     id: "",
@@ -59,18 +51,17 @@ export class UserService {
         if (err) console.log(err);
         console.log("Success");
       });
-    //console.log(form.value);
   }
 
   getUser(): Observable<any> {
-    this.id = this.currentUserSubject.value._id;
-    //console.log(this.id);
-    return this.http.get<User>(userUrl + "5cd7393868b4e90a3c1318a7", {
+    this.id = localStorage.getItem("_id");
+    return this.http.get<User>(userUrl + this.id, {
       headers: httpheaders
     });
   }
 
   login(form: NgForm): Observable<any> {
+    this.logout();
     this.loginUser = {
       email: form.value.email,
       password: form.value.password
@@ -78,34 +69,16 @@ export class UserService {
     return this.http
       .post<any>(loginUrl, JSON.stringify(this.loginUser), {
         headers: httpheaders
-      })
-      .pipe(
-        map(user => {
-          // login successful if there's a jwt token in the response
-          if (user && user.token) {
-            // store user details and jwt token in local storage to keep user logged in between page refreshes
-            localStorage.setItem("currentUser", JSON.stringify(user));
-            this.currentUserSubject.next(user);
-          }
-
-          return user;
-        })
-      );
-  }
-
-  public get currentUserValue() {
-    return this.currentUserSubject.value;
+      });
   }
 
   logout() {
-    localStorage.removeItem("currentUser");
-    localStorage.removeItem("token");
-    this.currentUserSubject.next(null);
+    localStorage.clear();
   }
 
   deleteUser(): Observable<any> {
-    //this.id =
-    return this.http.delete<User>(userUrl + "5cd2b7e252528710788d7020", {
+    this.id = localStorage.getItem("_id");
+    return this.http.delete<User>(userUrl + this.id, {
       headers: httpheaders
     });
   }
