d929fc0c0f02122ab8a94e5bff2ee62d4e723f7f
Alexandra Nadova <alexandranadova@gmail.com>
2019-05-14 18:31:37 +0200

    update header info method added

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -5,6 +5,7 @@ import { UserService } from "../user.service";
 import { User, Education, Experience } from "src/app/models/user";
 import { FooterService } from "src/app/footer/footer.service";
 import { NotifierService } from "angular-notifier";
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: "app-profile",
@@ -25,6 +26,14 @@ export class ProfileComponent implements OnInit {
     education: [] = [],
     workExperience: [] = []
   };
+  headerInfo: [
+    { propFirstName: "firstName"; value: "" },
+    { propFirstName: "lastName"; value: "" },
+    { propCountry: "country"; value: "" },
+    { propCity: "city"; value: "" }
+    //{ propFieldOfFocus: "fieldOfFocus"; value: "" }
+  ];
+  
   currentUser;
   education: Education[] = [];
   experience: Experience[] = [];
@@ -69,5 +78,54 @@ export class ProfileComponent implements OnInit {
   ngOnInit() {
     this.footer.hide();
     this.getUserProfile();
+  }
+
+  updateHeader(form: NgForm) {
+    console.log(this.user);
+    console.log(form.value.firstName);
+    this.headerInfo= [
+      {
+        propFirstName: "firstName",
+        value:
+          !form.value.firstName === undefined
+            ? this.user.firstName
+            : form.value.firstName
+      },
+      {
+        propFirstName: "lastName",
+        value:
+          !form.value.lastName === undefined
+            ? form.value.lastName
+            : this.user.lastName
+      },
+      {
+        propCountry: "country",
+        value:
+          !form.value.country === undefined
+            ? form.value.country
+            : this.user.country
+      },
+      {
+        propCity: "city",
+        value: !form.value.city === undefined ? form.value.city : this.user.city
+      },
+      // {
+      //   propFieldOfFocus: "fieldOfFocus",
+      //   value:
+      //     !form.value.fieldOfFocus === undefined
+      //       ? form.value.firstName
+      //       : this.user.fieldOfFocus
+      // }
+    ];
+    console.log(this.headerInfo);
+    this.userService.updateUser(this.headerInfo).subscribe(
+      res => {
+        this.notifier.notify(
+          "success",
+          "Your information was successfully updated"
+        );
+      },
+      err => this.notifier.notify("error", "Error occured: " + err.message)
+    );
   }
 }
