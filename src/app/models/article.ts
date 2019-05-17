0ed02f4ec2179709cf02d4d2630c4b2e704b1c57
Alexandra Nadova <alexandranadova@gmail.com>
2019-05-17 14:54:17 +0200

    article model

---
+++ src/app/models/article.ts
@@ -0,0 +1,13 @@
+export class Article {
+  constructor(
+    public _id: string,
+    public title: string, //required
+    public author: Array<string>, //required
+    public description: string,
+    //keywords: { type: Array },
+    //articleImages: Array<string>,,
+    public titleImage: string,
+    //category: string,
+    public content: string
+  ) {}
+}
