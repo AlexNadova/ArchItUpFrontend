d4f97124870029ec1f68096a096cdd7abf43e921
Alexandra Nadova <alexandranadova@gmail.com>
2019-05-18 22:22:19 +0200

    create article component added

--- src/app/article/article.module.ts
+++ src/app/article/article.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 
 import { ArticleRoutingModule } from "./article-routing.module";
 import { ArticlesComponent } from "./articles/articles.component";
 import { ArticleService } from "./article.service";
 import { ArticleComponent } from "./article/article.component";
+import { CreateArticleComponent } from "./create-article/create-article.component";
 
 @NgModule({
-  declarations: [ArticlesComponent, ArticleComponent],
-  imports: [CommonModule, ArticleRoutingModule],
+  declarations: [ArticlesComponent, ArticleComponent, CreateArticleComponent],
+  imports: [CommonModule, FormsModule, ArticleRoutingModule],
   providers: [ArticleService]
 })
 export class ArticleModule {}
