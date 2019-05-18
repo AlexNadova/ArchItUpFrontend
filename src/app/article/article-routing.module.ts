85b8bbf4dc69777fd92f965662190bc37350cd53
Alexandra Nadova <alexandranadova@gmail.com>
2019-05-18 22:22:09 +0200

    path for create added

--- src/app/article/article-routing.module.ts
+++ src/app/article/article-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { ArticlesComponent } from "./articles/articles.component";
 import { ArticleComponent } from './article/article.component';
+import { CreateArticleComponent } from './create-article/create-article.component';
+import { AuthGuard } from '../authentication/auth-guard';
 const routes: Routes = [
   {
     path: "",
@@ -15,6 +17,14 @@ const routes: Routes = [
     component: ArticleComponent,
     data: {
       title: "Articles"
+    }
+  },
+  {
+    path: "create",
+    component: CreateArticleComponent,
+    canActivate: [AuthGuard],
+    data: {
+      title: "Add article",     
     }
   }
 ];
